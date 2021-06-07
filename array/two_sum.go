73566512531cb7525039283e7713ee4f233c2572
LibinZhang <zhanglibin@yonyou.com>
2021-06-07 16:03:10 +0800

    two sum

---
+++ array/two_sum.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+//在数组中找到 2 个数之和等于给定值的数字，结果返回 2 个数字在数组中的下标。
+
+func main() {
+	arr := []int{1, 2, 4, 8, 7}
+	target := 15
+	result := twoSum(arr, target)
+	fmt.Printf("%+v\n", result)
+}
+
+//巧用map降低时间复杂度到o(n)
+func twoSum(arr []int, target int) []int {
+	m := make(map[int]int, len(arr))
+	for i, _ := range arr {
+		t1 := target - arr[i]
+		if _, ok := m[t1]; ok {
+			return []int{m[t1], i}
+		}
+		m[arr[i]] = i
+	}
+
+	return []int{-1, -1}
+}
